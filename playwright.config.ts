cd0fecd2233e3f76cf0eccf163771a9942bba0af
Dhende-avinash <dhendeavinash413@gmail.com>
2026-05-28 22:59:28 +0530

    "added Allure Report"

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,7 +23,10 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    ['html'],
+    ['allure-playwright']
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
@@ -35,8 +38,8 @@ export default defineConfig({
     //video: 'off',
 
     launchOptions: {
-    slowMo: 1500
-  },
+      slowMo: 1500
+    },
   },
 
   /* Configure projects for major browsers */
@@ -46,7 +49,7 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-    {
+    /*{
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
     },
@@ -54,7 +57,7 @@ export default defineConfig({
     {
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
-    },
+    },*/
 
     /* Test against mobile viewports. */
     // {
@@ -83,5 +86,5 @@ export default defineConfig({
   //   url: 'http://localhost:3000',
   //   reuseExistingServer: !process.env.CI,
   // },
-  
+
 });
